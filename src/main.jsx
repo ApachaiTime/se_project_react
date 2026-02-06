0b326093b359876ae60137ad3fcaea1280f72966
apachaitime <apachaitime@gmail.com>
2026-02-05 20:47:21 -0600

    adjusted base-name

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "../src/components/App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/se_project_react/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
